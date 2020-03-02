b057d01d18623478e66e77ddf315e193ff32b5e8
pkouliev <pkouliev@gmail.com>
2020-03-02 16:05:02 -0500

    JumpToTheNewWindow

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -8,19 +8,27 @@ import java.util.Set;
 
 public class JumpToTheNewWindow {
     public static void main(String[] args) throws Exception{
+
         WebDriverManager.chromedriver().setup();
+
         WebDriver driver = new ChromeDriver();
+
         driver.get("http://practice.cybertekschool.com/open_new_tab");
+
         Thread.sleep(5000);
+
         //every window has some id, this id calls window handle
         //based on window handle, we can switch in between windows
         String windowHandle = driver.getWindowHandle();
         System.out.println(windowHandle);
+
         //getWindowHandles() - returns id's of all currently opened windows
         //Set - doesn't allow duplicates
         Set<String> windowHandles = driver.getWindowHandles();
+
         System.out.println(windowHandles);
         System.out.println("BEFORE SWITCH :"+driver.getCurrentUrl());
+
         //since we have all windows
         //and we know id of original window
         //we can say switch to something that is not equals to old window id
@@ -31,8 +39,9 @@ public class JumpToTheNewWindow {
                 driver.switchTo().window(windowId);
             }
         }
+
         System.out.println("AFTER SWITCH :"+driver.getCurrentUrl());
-        // break until 5:02
+
 //        driver.close();
         driver.quit();
     }
