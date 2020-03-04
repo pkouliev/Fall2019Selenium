b85235d0b57faafd8bdd3e57ba3ff539db0623a6
pkouliev <pkouliev@gmail.com>
2020-03-04 16:38:19 -0500

    Added WebElement called message to find value by using class name locator and utilizing .getText() method to retrieve actual message.

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -28,9 +28,15 @@ public class FindElementsPractice {
         WebElement signUp = driver.findElement(By.name("wooden_spoon"));
         // when you see type="submit", you can use submit() instead of click()
         // it make sense to use when click() method doesn't work
-        signUp.submit();
+        signUp.click();
 
         Thread.sleep(2000);
+
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+
+        WebElement message = driver.findElement(By.className("subheader"));
+
+        String actual = message.getText();
 
 
         driver.quit(); // to close everything
