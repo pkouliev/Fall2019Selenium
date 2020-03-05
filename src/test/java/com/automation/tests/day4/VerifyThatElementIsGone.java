0b1ce0232d7d39d155d8b6ca8656735a50094ae0
pkouliev <pkouliev@gmail.com>
2020-03-05 18:02:15 -0500

    VerifyThatElementIsGone

--- src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
+++ src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
@@ -3,7 +3,10 @@ package com.automation.tests.day4;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class VerifyThatElementIsGone {
 
@@ -25,7 +28,9 @@ public class VerifyThatElementIsGone {
 
         Thread.sleep(2000);
 
-        if(driver.findElements(By.id("disappearing_button")).size() == 0) {
+        List<WebElement> list  = driver.findElements(By.id("disappearing_button"));
+
+        if(list.size() == 0) {
             System.out.println("TEST PASSED");
         }else {
             System.out.println("TEST FAILED");
