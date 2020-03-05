63a611dfec0335f283cb35eeed5cd9be03aec009
pkouliev <pkouliev@gmail.com>
2020-03-04 20:56:54 -0500

    Negative Testing is added with error message

--- src/test/java/com/automation/tests/day3/FindElementById.java
+++ src/test/java/com/automation/tests/day3/FindElementById.java
@@ -51,6 +51,20 @@ public class FindElementById {
         logout.click();
         Thread.sleep(2000);
 
+        // let's enter invalid credentials
+
+        driver.findElement(By.name("username")).sendKeys("wrong");
+        driver.findElement(By.name("password")).sendKeys("wrong");
+        driver.findElement(By.id("wooden_spoon")).click();
+
+        Thread.sleep(2000);
+
+        WebElement errorMessage = driver.findElement(By.id("flash-messages"));
+
+        System.out.println(errorMessage.getText());
+
+        Thread.sleep(2000);
+
         driver.quit();
 
     }
