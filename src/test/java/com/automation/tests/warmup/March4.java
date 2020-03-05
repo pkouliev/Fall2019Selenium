4dfb6141fdafc7530afea8cf10ee04ef496e2402
pkouliev <pkouliev@gmail.com>
2020-03-05 12:08:53 -0500

    Warm Up Exercise March 4, 2020

---
+++ src/test/java/com/automation/tests/warmup/March4.java
@@ -0,0 +1,23 @@
+package com.automation.tests.warmup;
+
+public class March4 {
+
+    static webDriver driver;
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void ebayTest() {
+
+    }
+
+    public static void amazonTest(){
+
+    }
+
+    public static void wikiTest() {
+
+    }
+}
