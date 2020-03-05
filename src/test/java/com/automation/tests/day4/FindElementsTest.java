5c5a2c9f9768f0669ea50dffb68d79171210e93c
pkouliev <pkouliev@gmail.com>
2020-03-05 15:37:03 -0500

    Testing for getting webElements

--- src/test/java/com/automation/tests/day4/FindElementsTest.java
+++ src/test/java/com/automation/tests/day4/FindElementsTest.java
@@ -1,8 +1,12 @@
 package com.automation.tests.day4;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class FindElementsTest {
 
@@ -15,6 +19,15 @@ public class FindElementsTest {
         Thread.sleep(3000);
 
         // how to collect all links from the page?
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        Thread.sleep(3000);
+
+        for(WebElement link: links) {
+            System.out.println(link.getText());
+        }
+
+        Thread.sleep(3000);
 
         driver.quit();
     }
