91d423de1478e41276157b74fff5ba7673e4f359
pkouliev <pkouliev@gmail.com>
2020-03-09 00:44:51 -0400

    CheckBoxes

--- src/test/java/com/automation/tests/day5/RadioButtonsTest.java
+++ src/test/java/com/automation/tests/day5/RadioButtonsTest.java
@@ -2,7 +2,6 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
