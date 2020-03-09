c3065ef4f3523927b5740081baec3cd2f85c4651
pkouliev <pkouliev@gmail.com>
2020-03-09 18:30:50 -0400

    findElement

--- src/test/java/com/automation/tests/day5/TestForiFrame.java
+++ src/test/java/com/automation/tests/day5/TestForiFrame.java
@@ -2,7 +2,9 @@ package com.automation.tests.day5;
 
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestForiFrame {
@@ -14,6 +16,10 @@ public class TestForiFrame {
         driver.get("http://practice.cybertekschool.com/iframe");
 
         BrowserUtils.wait(4);
+
+        WebElement textInput = driver.findElement(By.id("tinymce"));
+
+        System.out.println(textInput.getText());
 
 
 
