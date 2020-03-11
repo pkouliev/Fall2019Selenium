876c4fd92df3dadb79acb7b30686fb83cc7c4dde
pkouliev <pkouliev@gmail.com>
2020-03-11 19:03:46 -0400

    create a webElement object for drop-down

--- src/test/java/com/automation/tests/day6/SelectByText.java
+++ src/test/java/com/automation/tests/day6/SelectByText.java
@@ -1,8 +1,40 @@
 package com.automation.tests.day6;
 
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class SelectByText {
 
     public static void main(String[] args) {
+
+        WebDriver driver = DriverFactory.createADriver("chrome");
+
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(2);
+
+        // create a webElement object for drop-down first
+        WebElement simpleDropdown = driver.findElement(By.id("dropdown"));
+        BrowserUtils.wait(2);
+
+        // provide webElement object into constructor
+        Select selectSimpleDropdown = new Select(simpleDropdown);
+        BrowserUtils.wait(2);
+
+        //select by visible text
+        selectSimpleDropdown.selectByVisibleText("Option 2");
+        BrowserUtils.wait(2);
+
+
+
+
+
+
+
+        driver.quit();
 
 
     }
