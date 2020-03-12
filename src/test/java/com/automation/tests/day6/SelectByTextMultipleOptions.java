da671c9695879fc8ea39984cb505822251237ed9
pkouliev <pkouliev@gmail.com>
2020-03-11 21:19:33 -0400

    Select languageselect = new Select(driver.findElement(By.name("Languages")));         //Whether this select element support selecting multiple options at the same time?         // This is done by checking the value of "multiple" attribute.         boolean isMultiple = languageselect.isMultiple();         System.out.println(isMultiple);

--- src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -2,7 +2,9 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SelectByTextMultipleOptions {
 
@@ -12,12 +14,18 @@ public class SelectByTextMultipleOptions {
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(1);
 
+        Select languageselect = new Select(driver.findElement(By.name("Languages")));
+        //Whether this select element support selecting multiple options at the same time?
+        // This is done by checking the value of "multiple" attribute.
+        boolean isMultiple = languageselect.isMultiple();
+        System.out.println(isMultiple);
 
 
 
 
 
 
+        BrowserUtils.wait(1);
         driver.quit();
     }
 }
