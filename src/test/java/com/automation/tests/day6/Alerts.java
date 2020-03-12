fb23b862530d74c61fa40e034f40c8592865662d
pkouliev <pkouliev@gmail.com>
2020-03-12 01:10:05 -0400

    driver.switchTo().alert().accept(); //to click OK         String expected = "You successfully clicked an alert";         String actual = driver.findElement(By.id("result")).getText();
            // it will fail, because there is typ0
            if(expected.equals(actual)) {
                System.out.println("TEST PASSED");
            } else {
                System.out.println("TEST FAILED");
                System.out.println("Expected: " +expected);
                System.out.println("Actual: " +actual);
            }

--- src/test/java/com/automation/tests/day6/Alerts.java
+++ src/test/java/com/automation/tests/day6/Alerts.java
@@ -23,7 +23,20 @@ public class Alerts {
 
         //to get the text from popup message
         String popupText = driver.switchTo().alert().getText();
+        System.out.println(popupText);
+
         driver.switchTo().alert().accept(); //to click OK
+        String expected = "You successfully clicked an alert";
+        String actual = driver.findElement(By.id("result")).getText();
+
+        // it will fail, because there is typ0
+        if(expected.equals(actual)) {
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: " +expected);
+            System.out.println("Actual: " +actual);
+        }
 
 
 
