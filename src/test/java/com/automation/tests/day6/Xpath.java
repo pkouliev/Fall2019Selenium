63470d8bf6ed54e24b9008ebdbfbfbece3fb4c1a
pkouliev <pkouliev@gmail.com>
2020-03-12 14:22:51 -0400

    //value can be inside '' or ""         // if you don't know the tag name, or want to skip tag name, use *         // for example //*[@onclick='button1()'] | * means any tag name         WebElement btn1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));         btn1.click();         BrowserUtils.wait(1);

--- src/test/java/com/automation/tests/day6/Xpath.java
+++ src/test/java/com/automation/tests/day6/Xpath.java
@@ -2,7 +2,9 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Xpath {
 
@@ -11,6 +13,14 @@ public class Xpath {
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
         BrowserUtils.wait(1);
+
+        //value can be inside '' or ""
+        // if you don't know the tag name, or want to skip tag name, use *
+        // for example //*[@onclick='button1()'] | * means any tag name
+        WebElement btn1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+        btn1.click();
+        BrowserUtils.wait(1);
+
 
 
 
