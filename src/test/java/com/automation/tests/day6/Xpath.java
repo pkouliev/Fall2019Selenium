01bf6a018fec826f5d88ce03567f68b34c0f4eb6
pkouliev <pkouliev@gmail.com>
2020-03-12 14:16:08 -0400

    Xpath

---
+++ src/test/java/com/automation/tests/day6/Xpath.java
@@ -0,0 +1,27 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class Xpath {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = DriverFactory.createADriver("chrome");
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        BrowserUtils.wait(1);
+
+
+
+
+
+
+
+
+
+
+        BrowserUtils.wait(1);
+        driver.quit();
+    }
+}
