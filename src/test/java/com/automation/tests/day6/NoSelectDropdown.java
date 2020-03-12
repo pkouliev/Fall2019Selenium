1cbc38272e793167241b790fa715f92be99f797d
pkouliev <pkouliev@gmail.com>
2020-03-11 22:58:00 -0400

    driver.findElement(By.id("dropdownMenuLink")).click(); // to expand dropdown         BrowserUtils.wait(2);
            //<a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
            driver.findElement(By.linkText("Amazon")).click(); // click on option

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -2,6 +2,7 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class NoSelectDropdown {
@@ -12,7 +13,13 @@ public class NoSelectDropdown {
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(1);
 
-        BrowserUtils.wait(1);
+        driver.findElement(By.id("dropdownMenuLink")).click(); // to expand dropdown
+        BrowserUtils.wait(2);
+
+        //<a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
+        driver.findElement(By.linkText("Amazon")).click(); // click on option
+
+        BrowserUtils.wait(3);
         driver.quit();
     }
 }
