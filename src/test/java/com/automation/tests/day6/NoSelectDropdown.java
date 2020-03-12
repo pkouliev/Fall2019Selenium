bc7b2158f795f10e0cb1ea1c8e677c86a6ad1bc5
pkouliev <pkouliev@gmail.com>
2020-03-11 23:37:08 -0400

    List<WebElement> allLinks = driver.findElements(By.className("dropdown-item"));
            for (WebElement eachLink: allLinks) {
                System.out.println(eachLink.getText()+" : "+eachLink.getAttribute("href"));
            }

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -4,6 +4,9 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class NoSelectDropdown {
 
@@ -14,12 +17,20 @@ public class NoSelectDropdown {
         BrowserUtils.wait(1);
 
         driver.findElement(By.id("dropdownMenuLink")).click(); // to expand dropdown
-        BrowserUtils.wait(2);
+        BrowserUtils.wait(1);
 
         //<a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
-        driver.findElement(By.linkText("Amazon")).click(); // click on option
+//        driver.findElement(By.linkText("Amazon")).click(); // click on option
+        BrowserUtils.wait(1);
 
-        BrowserUtils.wait(3);
+        List<WebElement> allLinks = driver.findElements(By.className("dropdown-item"));
+
+        for (WebElement eachLink: allLinks) {
+            System.out.println(eachLink.getText()+" : "+eachLink.getAttribute("href"));
+        }
+
+        driver.findElement(By.linkText("Etsy")).click(); // click on option
+        BrowserUtils.wait(1);
         driver.quit();
     }
 }
