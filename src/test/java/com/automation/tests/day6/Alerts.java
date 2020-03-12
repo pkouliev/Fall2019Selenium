50f3ab883ffed5725e531e61f793299b557e5ee0
pkouliev <pkouliev@gmail.com>
2020-03-12 01:02:54 -0400

    List<WebElement> buttons = driver.findElements(By.tagName("button"));
            buttons.get(0).click(); //to click on the first button
            BrowserUtils.wait(1);
    
            //to get the text from popup message
            String popupText = driver.switchTo().alert().getText();
            driver.switchTo().alert().accept(); //to click OK

--- src/test/java/com/automation/tests/day6/Alerts.java
+++ src/test/java/com/automation/tests/day6/Alerts.java
@@ -2,7 +2,11 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Alerts {
 
@@ -11,6 +15,15 @@ public class Alerts {
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/javascript_alerts");
         BrowserUtils.wait(1);
+
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+
+        buttons.get(0).click(); //to click on the first button
+        BrowserUtils.wait(1);
+
+        //to get the text from popup message
+        String popupText = driver.switchTo().alert().getText();
+        driver.switchTo().alert().accept(); //to click OK
 
 
 
