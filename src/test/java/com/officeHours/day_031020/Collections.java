015ebb5227a0629a018543d8cc8d997c0cc453d1
pkouliev <pkouliev@gmail.com>
2020-03-13 20:25:31 -0400

    day 7: locators - Xpath

--- src/test/java/com/officeHours/day_031020/Collections.java
+++ src/test/java/com/officeHours/day_031020/Collections.java
@@ -48,6 +48,54 @@ public class Collections {
             System.out.println(iteratorSet.next());
         }
 
+        ArrayList<Integer> dropdown = new ArrayList<>();
+        dropdown.add(1);
+        dropdown.add(2);
+        dropdown.add(3);
+        dropdown.add(4);
+        dropdown.add(5);
+        dropdown.add(1);
+        // [1, 2, 3, 4, 5]
+        System.out.println("list: " + dropdown);
+
+        HashSet <Integer> optionsSet = new HashSet<>(dropdown);
+        System.out.println("optionsSet: " + optionsSet);
+
+        if (optionsSet.size() == dropdown.size()) {
+            System.out.println("expected is the same as actual");
+        } else {
+            System.out.println("options contains duplicates");
+        }
+
+        //==================================================================
+
+        /*
+            Queue
+            - PriorityQueue - First in first out
+                can accept duplicates, does not have indexes
+            - ArrayDequeue - special in adding and removing
+        */
+
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add("John");
+        priorityQueue.add("Kate");
+        priorityQueue.add("Tom");
+        System.out.println("Queue: " + priorityQueue);
+
+        System.out.println("peek: " + priorityQueue.peek());
+        //peeking (.peek()) - it will check and return the first in a queue
+        System.out.println("queue after peek: " + priorityQueue);
+
+        System.out.println("poll: " + priorityQueue.poll());
+        System.out.println("queue after poll: " + priorityQueue);
+        // polling will return the first value in a queue and remove it
+
+        ArrayDeque<String> deque = new ArrayDeque<>();
+
+
+
+
+
 
     }
 }
