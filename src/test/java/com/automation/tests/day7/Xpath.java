0d08053cebb701a847b304ae634c2b6996a1f5e3
pkouliev <pkouliev@gmail.com>
2020-03-13 20:30:06 -0400

    static String userNameLocator = "//label[text()=’Username’]/following-sibling::input";

--- src/test/java/com/automation/tests/day7/Xpath.java
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -2,15 +2,19 @@ package com.automation.tests.day7;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Xpath {
+
+    static String userNameLocator = "//label[text()=’Username’]/following-sibling::input";
 
     public static void main(String[] args) {
 
         WebDriver driver = DriverFactory.createADriver("chrome");
         driver.get("http://practice.cybertekschool.com/login");
         BrowserUtils.wait(3);
+        driver.findElement(By.xpath(userNameLocator));
 
 
 
