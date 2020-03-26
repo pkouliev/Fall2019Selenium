e955d0d1a929f62f950e2ddd32fe991d3ca12c2d
pkouliev <pkouliev@gmail.com>
2020-03-25 22:26:56 -0400

    TEST elementToBeClickableTest

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -4,13 +4,14 @@ import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.text.DecimalFormat;
 
 public class ExplicitWait {
 
@@ -56,6 +57,29 @@ public class ExplicitWait {
 
         wait.until(ExpectedConditions.visibilityOf(submitBtn));
         wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void elementToBeClickableTest() {
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/5");
+
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("loadingoverlay")));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn));
+
+        username.sendKeys("tomsmith");
+        password.sendKeys("SuperSecretPassword");
+        submitBtn.click();
 
         String expected = "Welcome to the Secure Area. When you are done click logout below.";
         String actual = driver.findElement(By.className("subheader")).getText();
