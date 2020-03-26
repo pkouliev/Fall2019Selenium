3d64c3d8f0b78063521e51856638dd608d24d6c1
pkouliev <pkouliev@gmail.com>
2020-03-25 20:52:37 -0400

    TEST waitForVisibility

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -1,10 +1,13 @@
 package com.automation.tests.day12;
 
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -31,6 +34,33 @@ public class ExplicitWait {
         driver.navigate().to("http://amazon.com");
 
         wait.until(ExpectedConditions.titleContains("Amazon"));
+    }
+
+    @Test
+    public void waitForVisibility() {
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+//        driver.findElement(By.tagName("button")).click();
+
+        // click on start button
+        driver.findElement(By.xpath("//button[text()='Start']")).click();
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.visibilityOf(username)).sendKeys("tomsmith");
+        wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("SuperSecretPassword");
+
+        wait.until(ExpectedConditions.visibilityOf(submitBtn));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.className("subheader")).getText();
+
+        Assert.assertEquals(actual, expected);
     }
 
     @AfterMethod
