00a8b56a771b1faf8c0936bc38bb894e5342ae57
pkouliev <pkouliev@gmail.com>
2020-03-30 15:36:18 -0400

    NewLoginTests

---
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -0,0 +1,35 @@
+package com.automation.tests.vytrack.login;
+
+import com.automation.pages.LoginPage;
+import com.automation.tests.vytrack.TestBase;
+import com.automation.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewLoginTests extends TestBase {
+
+
+    /**
+     * Login and verify that page title is "Dashboard"
+     */
+    @Test
+    public void verifyPageTitle() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+
+
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+    }
+
+    /**
+     * Enter wrong credentials and verify warning message
+     */
+
+    @Test
+    public void verifyWarningMessage() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("wrong", "wrong");
+        Assert.assertEquals(loginPage.getWarningMessage(), "Invalid user name or password.");
+    }
+
+}
