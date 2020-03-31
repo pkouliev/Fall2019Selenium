42ff90d60a978ad4574809ae7ab3d5fa603a88bf
pkouliev <pkouliev@gmail.com>
2020-03-31 15:53:53 -0400

    Update

--- src/test/java/com/automation/tests/homework/homework4/ValidLinks.java
+++ src/test/java/com/automation/tests/homework/homework4/ValidLinks.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
